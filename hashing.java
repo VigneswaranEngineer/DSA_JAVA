a1ec5756b8bdffdd8933f59a2f57e594cad9a3bf
VignewaranEngineer <vigneswaranengineer3063@gmail.com>
2024-03-17 14:02:53 +0530

    adding elements into hash set basic program using collections

---
+++ hashing.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class hashing {
+    public static void main(String[] args) {
+        HashSet<String> h1 = new HashSet<>();
+        h1.add("rakesh");
+        h1.add("tharun karthi ");
+        h1.add("zaid ");
+        h1.add("vignesh");
+        h1.add("varun karthick");
+        System.out.println(h1);
+  }
+}
